c946cdd7db5d8ef018f07df5dffb9c030d9d7e16
dfuruya <dfuruya@gmail.com>
2016-04-04 15:43:25 -0700

    Test circle transforms on svg

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
-var middle = 0;
+var center = {
+  x: width / 2,
+  y: height / 2
+};
 
-var svg = d3.select("body").append("svg")
-            .attr("width", width)
-            .attr("height", height);
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-svg.append("circle")
+var initData = [0, 0, 0, 0, 0, 0, 0, 0, 
+                0, 0, 0, 0, 0, 0, 0, 0, 
+                0, 0, 0, 0, 0, 0, 0, 0];
+
+var circles = svg.selectAll("circle")
+  .data(initData)
+  .enter()
+  .append("circle")
+  .attr("class", "dots")
   .attr("r", 20)
-  .attr("cx", 500)
-  .attr("cy", 500);
+  .attr("cx", center.x)
+  .attr("cy", center.y);
 
+
+var moveDots = function() {
+  
+  var weatherData = initData.slice().forEach(function(pos) {
+    return pos + 200 * Math.random();
+  });
+  console.log(weatherData);
+
+  svg.selectAll(".dots")
+    // .data(weatherData)
+    // .enter()
+    .transition()
+    .duration(500)
+    .attr("cx", function(d) {
+      return d + 500 * Math.random();
+    })
+    .attr("cy", function(d) {
+      return d + 500 * Math.random();
+    });
+};
+
+setTimeout(function() {
+  moveDots();
+}, 500);
