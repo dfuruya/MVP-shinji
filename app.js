7a44e0fd8ed0c4d23e540e084b787e73c1e082e6
dfuruya <dfuruya@gmail.com>
2016-04-04 12:21:53 -0700

    Add a test circle to "svg", and create fake data.csv

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
+var middle = 0;
 
 var svg = d3.select("body").append("svg")
             .attr("width", width)
-            .attr("height", height)
-            .append("g");
+            .attr("height", height);
+
+svg.append("circle")
+  .attr("r", 20)
+  .attr("cx", 500)
+  .attr("cy", 500);
+
