e2bd139e98534ea62fc2def83d4337d378948d8e
dfuruya <dfuruya@gmail.com>
2016-04-04 11:38:03 -0700

    Add style.css and "svg" to page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-// var svg = d3.select("body").append("svg")
-//             .
+var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g");
